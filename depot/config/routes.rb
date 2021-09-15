386f10cd5739725f5c70e24568210410cbfee2a9
Marcelo <santosmarcelo1995@live.com>
2021-09-14 21:34:22 -0300

    Add lang select

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     resources :orders
     resources :line_items
     resources :carts
-    root 'store#index', as: 'store_index'
+    root 'store#index', as: 'store_index', via: [:get, :post]
   end  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
